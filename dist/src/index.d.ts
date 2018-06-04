9fa4a51f558f3f0eabe43f2e5b9fb2b8cea5f773
dennisat <info@anel.co>
2018-06-04 06:53:11 +0200

    last minute changes

--- dist/src/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { DynaPlainRadioButton, IDynaPlainRadioButtonProps } from "./DynaPlainRadioButton";
-export { DynaRadioButton, IDynaRadioButtonProps } from "./DynaRadioButton";
-export { ESize, EStyle, EColor, EMode } from "dyna-ui-field-wrapper";
